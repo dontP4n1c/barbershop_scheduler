aff9936029519754d7c21c062aba813276a57816
Guilherme C <queijoprato33@gmail.com>
2025-11-01 23:47:04 -0300

    coment

--- frontend/src/components/BarberList.jsx
+++ frontend/src/components/BarberList.jsx
@@ -36,3 +36,4 @@ export default function BarberList() {
     </div>
   )
 }
+//comentarios
--- frontend/src/components/BookingForm.jsx
+++ frontend/src/components/BookingForm.jsx
@@ -70,3 +70,4 @@ export default function BookingForm({ barber, onDone }) {
     </div>
   )
 }
+//comentarios
